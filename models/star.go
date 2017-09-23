5fc8ed6562f020bd5352f53eadf30b7b0781e351
rezandry <andriyunantoreza@gmail.com>
2017-09-23 20:42:01 +0700

    add photo in star

--- models/star.go
+++ models/star.go
@@ -4,6 +4,7 @@ package models
 type Star struct {
 	ID      uint   `json:"id"`
 	Name    string `json:"name"`
+	Photo   string `json:"photo"`
 	Job     string `json:"job"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
